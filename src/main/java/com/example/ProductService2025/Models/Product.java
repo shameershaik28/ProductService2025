3d991fcd44e1632f7872e883f595029e8e6878b8
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2025-10-25 17:37:19 +0530

    Added spring data jpa to project

--- src/main/java/com/example/ProductService2025/Models/Product.java
+++ src/main/java/com/example/ProductService2025/Models/Product.java
@@ -1,11 +1,16 @@
 package com.example.ProductService2025.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product {
+    @Id
+    private long id;
     String name;
     String Category;
     String description;
